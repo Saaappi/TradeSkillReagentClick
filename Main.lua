b78322d90d8fbac0b16feda157c9a792b85a6ad7
Saaappi <alex.metz1024@gmail.com>
2025-06-08 14:00:59 -0700

    Changed the version to 1.0.0. All intended functionality should now be implemented.

--- Main.lua
+++ Main.lua
@@ -1,51 +1,89 @@
 local addonTable = select(2, ...)
-local prevButton, nextButton
 addonTable.pos = 0
 addonTable.recipes = {}
+addonTable.allRecipes = {}
 addonTable.ignoreSelection = false
+addonTable.itemToRecipeCache = {}
+
+local prevButton, nextButton
 
 local function AddRecipeToHistory(recipeID)
-    -- Truncate history if not at end
     if addonTable.pos < #addonTable.recipes then
         for i = #addonTable.recipes, addonTable.pos + 1, -1 do
             table.remove(addonTable.recipes, i)
         end
     end
-    -- Add new recipe and move pointer
     table.insert(addonTable.recipes, recipeID)
     addonTable.pos = #addonTable.recipes
 end
 
+local function FindRecipeIDByItemID(itemID)
+    if not itemID then return nil end
+    local cached = addonTable.itemToRecipeCache[itemID]
+    if cached then return cached end
+    for _, r in ipairs(addonTable.allRecipes) do
+        if r.itemID == itemID then
+            addonTable.itemToRecipeCache[itemID] = r.recipeID
+            return r.recipeID
+        end
+    end
+    return nil
+end
+
+local function CreateNavButton(name, parent, point, relativeTo, relativePoint, x, y, textureAtlas, direction)
+    local btn = CreateFrame("Button", name, parent)
+    btn:SetSize(20, 20)
+    btn:SetPoint(point, relativeTo, relativePoint, x, y)
+    btn.texture = btn:CreateTexture()
+    btn.texture:SetAtlas(textureAtlas)
+    btn:SetNormalTexture(btn.texture)
+    btn:SetHighlightAtlas(textureAtlas, "ADD")
+    btn:SetFrameLevel(1000)
+    btn:SetScript("OnEnter", function(self)
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+        GameTooltip:SetText("Click to go " .. direction, 1, 1, 1, 1, true)
+        GameTooltip:Show()
+    end)
+    btn:SetScript("OnLeave", function() GameTooltip:Hide() end)
+    return btn
+end
+
 EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
-    if ProfessionsFrame.CraftingPage.SchematicForm then
-        C_Timer.After(0.15, function()
-            local schematicForm = ProfessionsFrame.CraftingPage.SchematicForm
-            local slots = schematicForm:GetSlots()
-            if slots then
-                for _, slot in ipairs(slots) do
-                    slot:SetCheckboxCallback(function()
-                        print("A")
-                    end)
+    addonTable.pos = 0
+    addonTable.recipes = {}
+    addonTable.allRecipes = {}
+    addonTable.itemToRecipeCache = {}
+
+    C_Timer.After(2, function()
+        local recipeIDs = C_TradeSkillUI.GetAllRecipeIDs()
+        for _, recipeID in ipairs(recipeIDs) do
+            local recipeInfo = C_TradeSkillUI.GetRecipeInfo(recipeID)
+            if recipeInfo.learned then
+                local outputLink = C_TradeSkillUI.GetRecipeItemLink(recipeID)
+                if outputLink then
+                    local itemID = C_Item.GetItemInfoInstant(outputLink)
+                    if itemID then
+                        table.insert(addonTable.allRecipes, { recipeID = recipeID, itemID = itemID })
+                        addonTable.itemToRecipeCache[itemID] = recipeID
+                    end
                 end
             end
-        end)
-    end
-    if not prevButton and not nextButton then
-        nextButton = CreateFrame("Button", nil, ProfessionsFrame)
-        nextButton:SetSize(20, 20)
-        nextButton:SetPoint("BOTTOM", ProfessionsFrame.CraftingPage.RecipeList.SearchBox, "TOP", 5, 10)
-        nextButton.texture = nextButton:CreateTexture()
-        nextButton.texture:SetAtlas("common-icon-forwardarrow")
-        nextButton:SetNormalTexture(nextButton.texture)
-        nextButton:SetHighlightAtlas("common-icon-forwardarrow", "ADD")
+        end
+    end)
 
-        nextButton:SetScript("OnEnter", function(self)
-            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-            GameTooltip:SetText("Click to go forward", 1, 1, 1, 1, true)
-            GameTooltip:Show()
-        end)
-        nextButton:SetScript("OnLeave", function() GameTooltip:Hide() end)
-        nextButton:SetScript("OnClick", function(self)
+    if not prevButton and not nextButton then
+        nextButton = CreateNavButton(
+            "ReagentClickNextButton",
+            ProfessionsFrame,
+            "RIGHT",
+            ProfessionsFrame.CraftingPage.SchematicForm.TrackRecipeCheckbox,
+            "LEFT",
+            -5,
+            0,
+            "common-icon-forwardarrow",
+            "forward"
+        )
+        nextButton:SetScript("OnClick", function()
             local testIndex = addonTable.pos + 1
             if addonTable.recipes[testIndex] then
                 addonTable.pos = testIndex
@@ -55,21 +93,18 @@ EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
             end
         end)
 
-        prevButton = CreateFrame("Button", "ReagentClickPrevButton", ProfessionsFrame)
-        prevButton:SetSize(20, 20)
-        prevButton:SetPoint("RIGHT", nextButton, "LEFT", -5, 0)
-        prevButton.texture = prevButton:CreateTexture()
-        prevButton.texture:SetAtlas("common-icon-backarrow")
-        prevButton:SetNormalTexture(prevButton.texture)
-        prevButton:SetHighlightAtlas("common-icon-backarrow", "ADD")
-
-        prevButton:SetScript("OnEnter", function(self)
-            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-            GameTooltip:SetText("Click to go back", 1, 1, 1, 1, true)
-            GameTooltip:Show()
-        end)
-        prevButton:SetScript("OnLeave", function() GameTooltip:Hide() end)
-        prevButton:SetScript("OnClick", function(self)
+        prevButton = CreateNavButton(
+            "ReagentClickPreviousButton",
+            ProfessionsFrame,
+            "RIGHT",
+            nextButton,
+            "LEFT",
+            -5,
+            0,
+            "common-icon-backarrow",
+            "back"
+        )
+        prevButton:SetScript("OnClick", function()
             local testIndex = addonTable.pos - 1
             if addonTable.recipes[testIndex] then
                 addonTable.pos = testIndex
@@ -84,12 +119,23 @@ EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
     end
 end)
 
-EventRegistry:RegisterCallback("ProfessionsFrame.Hide", function()
-    addonTable.pos = 0
-    addonTable.recipes = {}
-end)
-
 EventRegistry:RegisterCallback("ProfessionsRecipeListMixin.Event.OnRecipeSelected", function(_, recipe)
+    C_Timer.After(0.15, function()
+        local schematicForm = ProfessionsFrame.CraftingPage.SchematicForm
+        local slots = schematicForm:GetSlots()
+        if slots then
+            for _, slot in ipairs(slots) do
+                slot.Button:HookScript("OnClick", function(self, key)
+                    if IsControlKeyDown() and key == "LeftButton" then
+                        local recipeID = FindRecipeIDByItemID(self.item)
+                        if recipeID then
+                            C_TradeSkillUI.OpenRecipe(recipeID)
+                        end
+                    end
+                end)
+            end
+        end
+    end)
     if addonTable.ignoreSelection then
         addonTable.ignoreSelection = false
         return
