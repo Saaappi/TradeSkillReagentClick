1efc40298d2132748dc94959b3e6d5a2266cda17
Saaappi <alex.metz1024@gmail.com>
2025-06-07 20:46:13 -0700

    Initial commit

---
+++ Main.lua
@@ -0,0 +1,171 @@
+local addonTable = select(2, ...)
+local prevButton, nextButton
+addonTable.pos = 0
+addonTable.recipes = {}
+addonTable.ignoreSelection = false
+
+local function AddRecipeToHistory(recipeID)
+    -- Truncate history if not at end
+    if addonTable.pos < #addonTable.recipes then
+        for i = #addonTable.recipes, addonTable.pos + 1, -1 do
+            table.remove(addonTable.recipes, i)
+        end
+    end
+    -- Add new recipe and move pointer
+    table.insert(addonTable.recipes, recipeID)
+    addonTable.pos = #addonTable.recipes
+    print("Added to history. Position:", addonTable.pos)
+end
+
+EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
+    if not prevButton and not nextButton then
+        nextButton = CreateFrame("Button", nil, ProfessionsFrame)
+        nextButton:SetSize(20, 20)
+        nextButton:SetPoint("BOTTOM", ProfessionsFrame.CraftingPage.RecipeList.SearchBox, "TOP", 5, 10)
+        nextButton.texture = nextButton:CreateTexture()
+        nextButton.texture:SetAtlas("common-icon-forwardarrow")
+        nextButton:SetNormalTexture(nextButton.texture)
+        nextButton:SetHighlightAtlas("common-icon-forwardarrow", "ADD")
+
+        nextButton:SetScript("OnEnter", function(self)
+            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+            GameTooltip:SetText("Click to go forward", 1, 1, 1, 1, true)
+            GameTooltip:Show()
+        end)
+        nextButton:SetScript("OnLeave", function() GameTooltip:Hide() end)
+        nextButton:SetScript("OnClick", function(self)
+            local testIndex = addonTable.pos + 1
+            if addonTable.recipes[testIndex] then
+                addonTable.pos = testIndex
+                print("Moved forward. Position:", addonTable.pos)
+                addonTable.ignoreSelection = true
+                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+                PlaySound(SOUNDKIT.IG_CHAT_SCROLL_UP, "Master")
+            end
+        end)
+
+        prevButton = CreateFrame("Button", "ReagentClickPrevButton", ProfessionsFrame)
+        prevButton:SetSize(20, 20)
+        prevButton:SetPoint("RIGHT", nextButton, "LEFT", -5, 0)
+        prevButton.texture = prevButton:CreateTexture()
+        prevButton.texture:SetAtlas("common-icon-backarrow")
+        prevButton:SetNormalTexture(prevButton.texture)
+        prevButton:SetHighlightAtlas("common-icon-backarrow", "ADD")
+
+        prevButton:SetScript("OnEnter", function(self)
+            GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+            GameTooltip:SetText("Click to go back", 1, 1, 1, 1, true)
+            GameTooltip:Show()
+        end)
+        prevButton:SetScript("OnLeave", function() GameTooltip:Hide() end)
+        prevButton:SetScript("OnClick", function(self)
+            local testIndex = addonTable.pos - 1
+            if addonTable.recipes[testIndex] then
+                addonTable.pos = testIndex
+                print("Moved backward. Position:", addonTable.pos)
+                addonTable.ignoreSelection = true
+                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+                PlaySound(SOUNDKIT.IG_CHAT_SCROLL_DOWN, "Master")
+            end
+        end)
+
+        nextButton:Show()
+        prevButton:Show()
+    end
+end)
+
+EventRegistry:RegisterCallback("ProfessionsFrame.Hide", function()
+    addonTable.pos = 0
+    addonTable.recipes = {}
+end)
+
+EventRegistry:RegisterCallback("ProfessionsRecipeListMixin.Event.OnRecipeSelected", function(_, recipe)
+    if addonTable.ignoreSelection then
+        addonTable.ignoreSelection = false
+        return
+    end
+    if recipe.learned then
+        AddRecipeToHistory(recipe.recipeID)
+    end
+end)
+
+--[[local function AddRecipeToHistory(recipeID)
+    -- If current position is not at the end, truncate the future history
+    if addonTable.pos < #addonTable.recipes then
+        for i = #addonTable.recipes, addonTable.pos + 1, -1 do
+            table.remove(addonTable.recipes, i)
+        end
+    end
+
+    -- Only add if it's not already the current recipe
+    if addonTable.recipes[addonTable.pos] ~= recipeID then
+        table.insert(addonTable.recipes, recipeID)
+        addonTable.pos = #addonTable.recipes
+        print("Position:", addonTable.pos)
+    end
+end
+
+EventRegistry:RegisterCallback("ProfessionsRecipeListMixin.Event.OnRecipeSelected", function(_, recipe)
+    if recipe.learned then
+        AddRecipeToHistory(recipe.recipeID)
+    end
+end)
+
+local function SlashHandler(msg)
+    local cmd = msg:match("^(%S*)%s*(.-)$")
+    cmd = cmd:lower()
+
+    if cmd == "next" then
+        local testIndex = addonTable.pos + 1
+        if addonTable.recipes[testIndex] then
+            addonTable.pos = testIndex
+            print("New Position:", addonTable.pos, " | Prev:", (addonTable.pos-1))
+            C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+        end
+    elseif cmd == "prev" then
+        local testIndex = addonTable.pos - 1
+        if addonTable.recipes[testIndex] then
+            addonTable.pos = testIndex
+            print("New Position:", addonTable.pos, " | Prev:", (addonTable.pos+1))
+            C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+        end
+    end
+end]]
+
+--[[local addonTable = select(2, ...)
+
+addonTable.pos = 0
+addonTable.recipes = {}
+
+EventRegistry:RegisterCallback("ProfessionsRecipeListMixin.Event.OnRecipeSelected", function(_, recipe)
+    if recipe.learned then
+        table.insert(addonTable.recipes, recipe.recipeID)
+        addonTable.pos = addonTable.pos + 1
+        print("Position:", addonTable.pos)
+    end
+end)
+
+local function SlashHandler(msg)
+    local cmd = msg:match("^(%S*)%s*(.-)$")
+    cmd = cmd:lower()
+
+    if cmd == "next" then
+        local testIndex = addonTable.pos + 1
+        if addonTable.recipes[testIndex] then
+            addonTable.pos = addonTable.pos + 1
+            print("New Position:", addonTable.pos, " | Prev:", (addonTable.pos-1))
+            if addonTable.pos and addonTable.recipes[addonTable.pos] then
+                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+            end
+        end
+    elseif cmd == "prev" then
+        local testIndex = addonTable.pos - 1
+        if addonTable.recipes[testIndex] then
+            addonTable.pos = addonTable.pos - 1
+            print("New Position:", addonTable.pos, " | Prev:", (addonTable.pos+1))
+            if addonTable.pos and addonTable.recipes[addonTable.pos] then
+                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
+            end
+        end
+    end
+end]]
