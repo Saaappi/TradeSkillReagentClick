cd2bfa6b5662d8dc9430d25c622dcb7209c84105
Saaappi <alex.metz1024@gmail.com>
2025-06-10 18:36:46 -0700

    Removed the navigation buttons. The scope of this addon is clicking reagents. The browsing history will go into another addon.

--- Main.lua
+++ Main.lua
@@ -5,8 +5,6 @@ addonTable.allRecipes = {}
 addonTable.ignoreSelection = false
 addonTable.itemToRecipeCache = {}
 
-local prevButton, nextButton
-
 local function AddRecipeToHistory(recipeID)
     if addonTable.pos < #addonTable.recipes then
         for i = #addonTable.recipes, addonTable.pos + 1, -1 do
@@ -28,24 +26,6 @@ local function FindRecipeIDByItemID(itemID)
         end
     end
     return nil
-end
-
-local function CreateNavButton(name, parent, point, relativeTo, relativePoint, x, y, textureAtlas, direction)
-    local btn = CreateFrame("Button", name, parent)
-    btn:SetSize(20, 20)
-    btn:SetPoint(point, relativeTo, relativePoint, x, y)
-    btn.texture = btn:CreateTexture()
-    btn.texture:SetAtlas(textureAtlas)
-    btn:SetNormalTexture(btn.texture)
-    btn:SetHighlightAtlas(textureAtlas, "ADD")
-    btn:SetFrameLevel(1000)
-    btn:SetScript("OnEnter", function(self)
-        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-        GameTooltip:SetText("Click to go " .. direction, 1, 1, 1, 1, true)
-        GameTooltip:Show()
-    end)
-    btn:SetScript("OnLeave", function() GameTooltip:Hide() end)
-    return btn
 end
 
 EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
@@ -70,53 +50,6 @@ EventRegistry:RegisterCallback("ProfessionsFrame.Show", function()
             end
         end
     end)
-
-    if not prevButton and not nextButton then
-        nextButton = CreateNavButton(
-            "ReagentClickNextButton",
-            ProfessionsFrame,
-            "RIGHT",
-            ProfessionsFrame.CraftingPage.SchematicForm.TrackRecipeCheckbox,
-            "LEFT",
-            -5,
-            0,
-            "common-icon-forwardarrow",
-            "forward"
-        )
-        nextButton:SetScript("OnClick", function()
-            local testIndex = addonTable.pos + 1
-            if addonTable.recipes[testIndex] then
-                addonTable.pos = testIndex
-                addonTable.ignoreSelection = true
-                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
-                PlaySound(SOUNDKIT.IG_CHAT_SCROLL_UP, "Master")
-            end
-        end)
-
-        prevButton = CreateNavButton(
-            "ReagentClickPreviousButton",
-            ProfessionsFrame,
-            "RIGHT",
-            nextButton,
-            "LEFT",
-            -5,
-            0,
-            "common-icon-backarrow",
-            "back"
-        )
-        prevButton:SetScript("OnClick", function()
-            local testIndex = addonTable.pos - 1
-            if addonTable.recipes[testIndex] then
-                addonTable.pos = testIndex
-                addonTable.ignoreSelection = true
-                C_TradeSkillUI.OpenRecipe(addonTable.recipes[addonTable.pos])
-                PlaySound(SOUNDKIT.IG_CHAT_SCROLL_DOWN, "Master")
-            end
-        end)
-
-        nextButton:Show()
-        prevButton:Show()
-    end
 end)
 
 EventRegistry:RegisterCallback("ProfessionsRecipeListMixin.Event.OnRecipeSelected", function(_, recipe)
